545e53d0271c6a09839901f009b4e14411981106
Asher Lee-Tyson <aleetyson@gmail.com>
2022-09-10 12:06:50 -0500

    Second Commit - computer choice created

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 console.log("Hello, World B");
+
+let computerChoiceArray = ['rock', 'paper', 'scissors'];
+let randomComputerChoice = Math.floor(Math.random()*computerChoiceArray.length);
+
+console.log(randomComputerChoice);
+
+let getComputerChoice = computerChoiceArray[randomComputerChoice];
+
+console.log(getComputerChoice);
